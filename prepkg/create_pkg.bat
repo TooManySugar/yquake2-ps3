0b317993333698d93ad92797d1446de9866bf894
TooManySugar <toomanysugar.game@tutanota.com>
2023-04-07 14:13:49 +0300

    Prepkg and pkg naming changed to new versioning form

--- prepkg/create_pkg.bat
+++ prepkg/create_pkg.bat
@@ -6,7 +6,7 @@ for /f "tokens=*" %%x in (.\version) do (
 )
 :read_version_end_loop
 
-set filename=yquake2_%version%
+set filename=yquake2-ps3_v%version%
 
 echo Creating %filename%.pkg it may take a while depending on .\pkg\ folder size...
 .\tools\pkg-win64.exe --contentid UP0001-QUAKE2_00-0000000000000000 .\pkg\ %filename%.pkg
--- prepkg/create_pkg.sh
+++ prepkg/create_pkg.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 version=$( cat version )
-filename=yquake2_$version
+filename=yquake2-ps3_v$version
 
 echo Creating $filename.pkg it may take a while depending on ./pkg/ folder size...
 ./tools/pkg-linux --contentid UP0001-QUAKE2_00-0000000000000000 ./pkg/ $filename.pkg
